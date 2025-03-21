6c0889c43cd66570742820ef96fa37703359dbec
sny <127958265+kyofu95@users.noreply.github.com>
2025-03-21 20:41:44 +0300

    update servise tests

--- tests/unit/test_proxy_service.py
+++ tests/unit/test_proxy_service.py
@@ -5,25 +5,28 @@ from uuid import uuid4
 import pytest
 import pytest_asyncio
 
+from app.core.uow import SQLUnitOfWork
 from app.models.proxy import Protocol, Proxy
 from app.service.proxy import ProxyService
 
 
 @pytest_asyncio.fixture(loop_scope="module", scope="module")
-async def mock_repo() -> AsyncMock:
-    return AsyncMock()
+async def mock_uow() -> AsyncMock:
+    uow = AsyncMock(SQLUnitOfWork)
+    uow.__aenter__.return_value = uow
+    uow.proxy_repository = AsyncMock()
+    uow.source_repository = AsyncMock()
+    return uow
 
 
 @pytest_asyncio.fixture(loop_scope="module", scope="module")
-async def service(mock_repo: AsyncMock) -> ProxyService:
-    service = ProxyService(async_session=None)
-    service.proxy_repository = mock_repo
-    return service
+async def service(mock_uow: AsyncMock) -> ProxyService:
+    return ProxyService(mock_uow)
 
 
 @pytest.mark.unit
 @pytest.mark.asyncio(loop_scope="module")
-async def test_create_proxy(service: ProxyService, mock_repo: AsyncMock) -> None:
+async def test_create_proxy(service: ProxyService, mock_uow: AsyncMock) -> None:
     proxy_id = uuid4()
     address = IPv4Address("192.168.1.1")
     port = 8080
@@ -35,44 +38,44 @@ async def test_create_proxy(service: ProxyService, mock_repo: AsyncMock) -> None
     mock_proxy.port = port
     mock_proxy.protocol = protocol
 
-    mock_repo.add.return_value = mock_proxy
+    mock_uow.proxy_repository.add.return_value = mock_proxy
     result = await service.create(address, port, protocol)
-
-    mock_repo.add.assert_called_once()
+    
+    mock_uow.proxy_repository.add.assert_called_once()
     assert result == mock_proxy
 
 
 @pytest.mark.unit
 @pytest.mark.asyncio(loop_scope="module")
-async def test_get_by_id(service: ProxyService, mock_repo: AsyncMock) -> None:
+async def test_get_by_id(service: ProxyService, mock_uow: AsyncMock) -> None:
     proxy_id = uuid4()
     mock_proxy = Proxy()
     mock_proxy.id = proxy_id
 
-    mock_repo.get_by_id.return_value = mock_proxy
+    mock_uow.proxy_repository.get_by_id.return_value = mock_proxy
     result = await service.get_by_id(proxy_id)
-
-    mock_repo.get_by_id.assert_called_once_with(proxy_id)
+    
+    mock_uow.proxy_repository.get_by_id.assert_called_once_with(proxy_id)
     assert result == mock_proxy
 
 
 @pytest.mark.unit
 @pytest.mark.asyncio(loop_scope="module")
-async def test_update_proxy(service: ProxyService, mock_repo: AsyncMock) -> None:
+async def test_update_proxy(service: ProxyService, mock_uow: AsyncMock) -> None:
     mock_proxy = Proxy()
-    mock_repo.update.return_value = mock_proxy
+    mock_uow.proxy_repository.update.return_value = mock_proxy
 
     result = await service.update(mock_proxy)
-
-    mock_repo.update.assert_called_once_with(mock_proxy)
+    
+    mock_uow.proxy_repository.update.assert_called_once_with(mock_proxy)
     assert result == mock_proxy
 
 
 @pytest.mark.unit
 @pytest.mark.asyncio(loop_scope="module")
-async def test_remove_proxy(service: ProxyService, mock_repo: AsyncMock) -> None:
+async def test_remove_proxy(service: ProxyService, mock_uow: AsyncMock) -> None:
     mock_proxy = Proxy()
 
     await service.remove(mock_proxy)
-
-    mock_repo.remove.assert_called_once_with(mock_proxy)
+    
+    mock_uow.proxy_repository.remove.assert_called_once_with(mock_proxy)
--- tests/unit/test_source_service.py
+++ tests/unit/test_source_service.py
@@ -4,24 +4,26 @@ from uuid import uuid4
 import pytest
 import pytest_asyncio
 
+from app.core.uow import SQLUnitOfWork
 from app.models.source import Source, SourceType
 from app.service.source import SourceService
 
+@pytest_asyncio.fixture(loop_scope="module", scope="module")
+async def mock_uow() -> AsyncMock:
+    uow = AsyncMock(SQLUnitOfWork)
+    uow.__aenter__.return_value = uow
+    uow.proxy_repository = AsyncMock()
+    uow.source_repository = AsyncMock()
+    return uow
+
 
 @pytest_asyncio.fixture(loop_scope="module", scope="module")
-async def mock_repo() -> AsyncMock:
-    return AsyncMock()
-
-
-@pytest_asyncio.fixture(loop_scope="module", scope="module")
-async def service(mock_repo: AsyncMock) -> SourceService:
-    service = SourceService(async_session=None)
-    service.source_repository = mock_repo
-    return service
+async def service(mock_uow: AsyncMock) -> SourceService:
+    return SourceService(mock_uow)
 
 
 @pytest.mark.asyncio
-async def test_create_source(service: SourceService, mock_repo: AsyncMock) -> None:
+async def test_create_source(service: SourceService, mock_uow: AsyncMock) -> None:
     source_id = uuid4()
     name = "Test Source"
     uri_type = None
@@ -32,41 +34,41 @@ async def test_create_source(service: SourceService, mock_repo: AsyncMock) -> No
     mock_source.uri_predefined_type = uri_type
     mock_source.type = SourceType.Text
 
-    mock_repo.add.return_value = mock_source
-    result = await service.create(name, uri_type)
-
-    mock_repo.add.assert_called_once()
+    mock_uow.source_repository.add.return_value = mock_source
+    result = await service.create(name)
+    
+    mock_uow.source_repository.add.assert_called_once()
     assert result == mock_source
 
 
 @pytest.mark.asyncio
-async def test_get_by_id(service: SourceService, mock_repo: AsyncMock) -> None:
+async def test_get_by_id(service: SourceService, mock_uow: AsyncMock) -> None:
     source_id = uuid4()
     mock_source = Source()
     mock_source.id = source_id
 
-    mock_repo.get_by_id.return_value = mock_source
+    mock_uow.source_repository.get_by_id.return_value = mock_source
     result = await service.get_by_id(source_id)
-
-    mock_repo.get_by_id.assert_called_once_with(source_id)
+    
+    mock_uow.source_repository.get_by_id.assert_called_once_with(source_id)
     assert result == mock_source
 
 
 @pytest.mark.asyncio
-async def test_update_source(service: SourceService, mock_repo: AsyncMock) -> None:
+async def test_update_source(service: SourceService, mock_uow: AsyncMock) -> None:
     mock_source = Source()
-    mock_repo.update.return_value = mock_source
+    mock_uow.source_repository.update.return_value = mock_source
 
     result = await service.update(mock_source)
-
-    mock_repo.update.assert_called_once_with(mock_source)
+    
+    mock_uow.source_repository.update.assert_called_once_with(mock_source)
     assert result == mock_source
 
 
 @pytest.mark.asyncio
-async def test_remove_source(service: SourceService, mock_repo: AsyncMock) -> None:
+async def test_remove_source(service: SourceService, mock_uow: AsyncMock) -> None:
     mock_source = Source()
 
     await service.remove(mock_source)
-
-    mock_repo.remove.assert_called_once_with(mock_source)
+    
+    mock_uow.source_repository.remove.assert_called_once_with(mock_source)
