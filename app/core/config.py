d03e4633eb34a3b08a36f8211dbc6f176634c362
sny <127958265+kyofu95@users.noreply.github.com>
2025-04-01 19:46:18 +0300

    Celery config

--- app/core/config.py
+++ app/core/config.py
@@ -26,3 +26,29 @@ class DatabaseSettings(BaseSettings):
 
 
 database_settings = DatabaseSettings()
+
+
+class CelerySettings(BaseSettings):
+    """
+    Configuration settings for Celery broker connection.  
+
+    Attributes:
+        broker (str): The message broker type (e.g., "redis", "rabbitmq"). Defaults to "redis".
+        user (str): The username for broker authentication. Defaults to an empty string.
+        password (str): The password for broker authentication. Defaults to an empty string.
+        host (str): The hostname or IP address of the broker. Defaults to "localhost".
+        port (int): The port number of the broker. Defaults to 6379.
+        name (str): The database name for the broker (if applicable). Defaults to "0".
+    """
+
+    broker: str = Field(alias="CELERY_BROKER_DRIVER", default="redis")
+    user: str = Field(alias="CELERY_BROKER_USER", default="")
+    password: str = Field(alias="CELERY_BROKER_PASSWORD", default="")
+    host: str = Field(alias="CELERY_BROKER_HOST", default="localhost")
+    port: int = Field(alias="CELERY_BROKER_PORT", default=6379)
+    name: str = Field(alias="CELERY_BROKER_NAME", default="0", description="database name")
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+celery_settings = CelerySettings()
