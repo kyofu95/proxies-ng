4b6ac654e233f0b80c8e423450bdb89f6d06e1a8
sny <127958265+kyofu95@users.noreply.github.com>
2025-04-07 18:35:29 +0300

    async runner with redis lock

--- app/tasks/celery.py
+++ app/tasks/celery.py
@@ -1,6 +1,13 @@
-from celery import Celery
+import asyncio
+from collections.abc import Callable, Coroutine
+from typing import Any
 
-from app.core.config import celery_settings
+from celery import Celery, current_task
+from redis.asyncio import ConnectionPool, Redis
+
+from app.core.config import celery_settings, redis_settings
+
+from .fetch_proxies import fetch_proxies
 
 
 def format_broker_url() -> str:
@@ -23,15 +30,61 @@ celery_broker = format_broker_url()
 
 celery_app = Celery("tasks", broker=celery_broker, backend=celery_broker)
 
+
+def get_redis() -> Redis:
+    """
+    Create and return a Redis client using the configured Redis settings.
+
+    Returns:
+        Redis: An asyncio-compatible Redis client.
+    """
+    pool = ConnectionPool.from_url(f"redis://{redis_settings.host}:{redis_settings.port}/1")
+    return Redis(connection_pool=pool, decode_responses=True)
+
+
 celery_app.conf.update(
     task_serializer="json",
     result_backend=celery_broker,
 )
 
+type Coro = Callable[[], Coroutine[Any, Any, None]]
+
+
+async def async_task_runner(func: Coro) -> None:
+    """
+    Run an async task with Redis-based locking to prevent concurrent execution.
+
+    This function ensures that only one instance of the given task is running at a time.
+    It uses Redis to acquire a lock and clears it once the task is done.
+
+    Args:
+        func (Coro): The coroutine function to run.
+
+    Raises:
+        ValueError: If the Celery task does not have an associated ID.
+    """
+    redis_key = f"lock:{func.__name__}"
+    task_id = current_task.request.id
+    if not task_id:
+        raise ValueError("No task id provided")
+
+    redis = get_redis()
+    if not await redis.set(redis_key, task_id, ex=300, nx=True):
+        # task already running
+        return
+
+    try:
+        await func()
+    finally:
+        current_value = await redis.get(redis_key)
+        if current_value == task_id:
+            await redis.delete(redis_key)
+
 
 @celery_app.task
 def get_proxy_sources_task() -> None:
     """Celery basic task to retrieve proxy sources."""
+    asyncio.run(async_task_runner(fetch_proxies))
 
 
 @celery_app.task
