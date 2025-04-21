7efb349bec99c666153adf1dffe77ecbb2e3bba7
sny <127958265+kyofu95@users.noreply.github.com>
2025-04-21 22:23:19 +0300

    paginate output of endpoint

--- app/api/v1/endpoints/proxy.py
+++ app/api/v1/endpoints/proxy.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Query, status
 from pydantic import TypeAdapter
 
-from .schemas.proxy import ProxyResponse
+from .schemas.proxy import PaginatedProxyResponse, ProxyResponse
 from .utils.di_deps import ProxyServiceDep
 
 router = APIRouter(prefix="/proxy", tags=["Proxy"])
@@ -16,22 +16,29 @@ async def get_proxies(
         str | None,
         Query(..., description="2-letter country code", min_length=2, max_length=2),
     ] = None,
-) -> list[ProxyResponse]:
+    offset: int | None = None,
+    limit: int = 100,
+) -> PaginatedProxyResponse:
     """
-    Retrieve a list of available proxies.
+    Retrieve a paginated list of available proxies.
 
-    This endpoint fetches all stored proxies from the database.
-    It can filter the proxies by the 2-letter country code and only return those marked as "checked".
+    This endpoint fetches proxies from the database with optional filtering by country code.
+    Only proxies that have been checked (i.e. tested at least once) are returned.
+    The results can be paginated using `offset` and `limit`.
 
     Args:
         proxy_service (ProxyServiceDep): Injected ProxyService dependency.
-        country_code (str | None): Optional 2-letter country code to filter proxies by country.
-            If not provided, all proxies will be returned.
+        country_code (str | None): Optional 2-letter ISO 3166-1 Alpha-2 country code to filter proxies by.
+        offset (int | None): Optional number of items to skip before returning results.
+        limit (int): Maximum number of proxies to return. Defaults to 100.
 
     Returns:
-        list[ProxyResponse]: A list of proxy objects with their connection and geolocation info.
+        PaginatedProxyResponse: A paginated response containing a list of proxies and metadata.
     """
     type_adapter = TypeAdapter(list[ProxyResponse])
-
     proxies = await proxy_service.get_proxies(country_alpha2_code=country_code, only_checked=True)
-    return type_adapter.validate_python(proxies)
+    validated_proxies = type_adapter.validate_python(proxies)
+
+    total_count = await proxy_service.get_proxies_count(country_alpha2_code=country_code, only_checked=True)
+
+    return PaginatedProxyResponse(proxies=validated_proxies, total=total_count, offset=offset, limit=limit)
