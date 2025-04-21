b554cd1237ff15fb9d9d485af15c3c3b1cb8f050
sny <127958265+kyofu95@users.noreply.github.com>
2025-04-21 15:27:32 +0300

    countries api

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from .v1.endpoints.country import router as country_router
 from .v1.endpoints.proxy import router as proxy_router
 
 api_router = APIRouter(prefix="/api")
 
+api_router.include_router(country_router)
 api_router.include_router(proxy_router)
---
+++ app/api/v1/endpoints/country.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, status
+
+from .utils.di_deps import ProxyServiceDep
+
+router = APIRouter(prefix="/country", tags=["country"])
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_countries(proxy_service: ProxyServiceDep) -> list[str]:
+    """
+    Retrieve a list of available countries.
+
+    Args:
+        proxy_service (ProxyServiceDep): The injected ProxyService dependency responsible for fetching the countries.
+
+    Returns:
+        list[str]: A list of country names or codes.
+    """
+    return await proxy_service.get_countries()
