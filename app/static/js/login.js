c130ef7698f076c4c7faf38040f16af0dbb703e7
sny <127958265+kyofu95@users.noreply.github.com>
2025-05-03 20:39:41 +0300

    login web page

---
+++ app/static/js/login.js
@@ -0,0 +1,37 @@
+$(function () {
+    $('#loginForm').on('submit', function (e) {
+        e.preventDefault();
+
+        const username = $('#loginInput').val();
+        const password = $('#passwordInput').val();
+
+        if (!username || !password) {
+            $('#loginError').text('Username and password are required.').removeClass('d-none');
+            return;
+        }
+
+        const data = new URLSearchParams();
+        data.append('username', username);
+        data.append('password', password);
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/private/auth/login',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            success: function () {
+                console.log('Redirect OK');
+            },
+            error: function (xhr) {
+                if (xhr.status === 401) {
+                    $('#loginError').text('Incorrect username or password.').removeClass('d-none');
+                } else {
+                    $('#loginError').text('Unexpected error occurred.').removeClass('d-none');
+                }
+            }
+        });
+    });
+});
