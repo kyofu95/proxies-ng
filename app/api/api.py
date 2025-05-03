f7f61032e5b88d2a4bdd680659c6b3d0213e0fec
sny <127958265+kyofu95@users.noreply.github.com>
2025-05-03 20:38:24 +0300

    include auth api as private

--- app/api/api.py
+++ app/api/api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .v1.endpoints.auth import router as auth_router
 from .v1.endpoints.country import router as country_router
 from .v1.endpoints.proxy import router as proxy_router
 
@@ -7,3 +8,9 @@ api_router = APIRouter(prefix="/api")
 
 api_router.include_router(country_router)
 api_router.include_router(proxy_router)
+
+private_api_router = APIRouter(prefix="/private")
+
+private_api_router.include_router(auth_router)
+
+api_router.include_router(private_api_router)
