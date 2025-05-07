69f6d925bbf7aa8d62373f4a9afe7620012a8e0e
sny <127958265+kyofu95@users.noreply.github.com>
2025-05-07 21:34:46 +0300

    use health api

---
+++ app/static/js/dashboard.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $.get("/api/health", function (data) {
+        $("#statusLabel").text("✔ " + data.status.toUpperCase());
+    }).fail(function (xhr, status, error) {
+        var errorMessage = xhr.responseJSON ? xhr.responseJSON.detail : "An error occurred";
+        $("#statusLabel").text("❗ Error: " + errorMessage);
+    });
+});
