f3965349b816c63cec62cc915278a251f9526974
sny <127958265+kyofu95@users.noreply.github.com>
2025-05-12 19:50:30 +0300

    preinit now only on dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Run the migrations
-alembic upgrade head
-
-python3 -m app.init_data
-
 if [[ "$DEBUG" == "true" ]]; then
     # Try to install debugger and run server with it
     pip install debugpy
     python3 -m debugpy --listen 0.0.0.0:5678 -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-level debug
 else
     # Run the server
-    uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-level debug
+    uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-level info
 fi
