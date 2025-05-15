8e85ad65830969b31b7bf701103aa5af48ceafed
sny <127958265+kyofu95@users.noreply.github.com>
2025-05-15 17:51:20 +0300

    default number of workers

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# get amount of workers from env or use default value of 3
+uvicorn_workers="${UVICORN_WORKERS:-3}"
+
 if [[ "$DEBUG" == "true" ]]; then
     # Try to install debugger and run server with it
     pip install debugpy
     python3 -m debugpy --listen 0.0.0.0:5678 -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-config uvicorn_logging.yml --log-level debug
 else
     # Run the server
-    uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-config uvicorn_logging.yml --log-level info
+    uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-config uvicorn_logging.yml --log-level info --workers "$uvicorn_workers"
 fi
