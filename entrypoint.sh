f40845350983219d96df409e1a98013340c5ba14
sny <127958265+kyofu95@users.noreply.github.com>
2025-05-20 20:56:30 +0300

    forwarded ips

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ uvicorn_workers="${UVICORN_WORKERS:-3}"
 if [[ "$DEBUG" == "true" ]]; then
     # Try to install debugger and run server with it
     pip install debugpy
-    python3 -m debugpy --listen 0.0.0.0:5678 -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-config uvicorn_logging.yml --log-level debug
+    python3 -m debugpy --listen 0.0.0.0:5678 -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --forwarded-allow-ips="*" --log-config uvicorn_logging.yml --log-level debug
 else
     # Run the server
-    uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-config uvicorn_logging.yml --log-level info --workers "$uvicorn_workers"
+    uvicorn app.main:app --host 0.0.0.0 --port 8000 --forwarded-allow-ips="*" --log-config uvicorn_logging.yml --log-level info --workers "$uvicorn_workers"
 fi
